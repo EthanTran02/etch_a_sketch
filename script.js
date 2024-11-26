5a42d4530dcaedd7a5c81a492f80783d1e996d82
Ethan <khoitranec@gmail.com>
2024-11-26 21:51:47 +0700

    create 16 * 16 grid

---
+++ script.js
@@ -0,0 +1,17 @@
+const container = document.querySelector('#container')
+
+for (let i = 1; i <= (16 * 16); i ++) {
+    const div = document.createElement('div')
+    const num = document.createElement('p')
+
+    num.textContent = `${i}`
+
+    container.appendChild(div)
+    div.appendChild(num)
+
+    div.setAttribute('style', 'position: relative')
+    num.setAttribute('style', 'position: absolute; top: 0; left: 0')
+}
+
+
+
