57131704a450ee3579424930dd02f31b93018257
Ethan <khoitranec@gmail.com>
2024-11-26 22:17:14 +0700

    set up hover effect change background of div

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const container = document.querySelector('#container')
 
+
 for (let i = 1; i <= (16 * 16); i ++) {
     const div = document.createElement('div')
     const num = document.createElement('p')
@@ -11,6 +12,14 @@ for (let i = 1; i <= (16 * 16); i ++) {
 
     div.setAttribute('style', 'position: relative')
     num.setAttribute('style', 'position: absolute; top: 0; left: 0')
+
+    div.addEventListener('mouseenter', (e) => {
+        e.target.style.backgroundColor = 'red'
+    })
+
+    div.addEventListener('mouseleave', (e) => {
+        e.target.style.backgroundColor = 'white'
+    })
 }
 
 
