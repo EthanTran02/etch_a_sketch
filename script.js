430c6b7c36c0b5f59c610773c21afbcccf8ee836
Ethan Tran <khoitranec@gmail.com>
2025-03-15 14:59:01 +0700

    fixed code readability

--- script.js
+++ script.js
@@ -1,52 +1,47 @@
-const container = document.querySelector('#container')
-const body = document.querySelector('body')
-const button = document.querySelector('button')
-const containerWidth =  500
+const container = document.querySelector("#container");
+const body = document.querySelector("body");
+const button = document.querySelector("button");
+const containerWidth = 500;
 
 function creatDivs(num) {
-    const totalBorderWidth = num //each divs have 0.5px border of each side
-    const divsSive = (containerWidth - totalBorderWidth) / num
-    container.innerHTML = ''
-    for (let i = 1; i <= (num * num); i ++) {
-        const div = document.createElement('div')
-        const num = document.createElement('p')
-    
-        container.appendChild(div)
-        div.appendChild(num)
-    
-        div.setAttribute('style', `position: relative; 
-            width: ${divsSive}px; 
-            height: ${divsSive}px;`);
-        num.setAttribute('style', 'position: absolute; top: 0; left: 0')
+  const totalBorderWidth = num; //each divs have 0.5px border of each side
+  const divsSive = (containerWidth - totalBorderWidth) / num;
+  container.innerHTML = "";
+  for (let i = 1; i <= num * num; i++) {
+    const div = document.createElement("div");
+    const num = document.createElement("p");
 
-        let opacity = 0
-        let red = Math.floor(Math.random() * 255)
-        let blue = Math.floor(Math.random() * 255)
-        let green = Math.floor(Math.random() * 255)
-        div.addEventListener('mouseenter', (e) => {
-            opacity += 0.1 
-            e.target.style.backgroundColor = `rgba(${red}, ${blue}, ${green},${opacity})`
-        })
-    
-        
-    }
+    container.appendChild(div);
+    div.appendChild(num);
+
+    div.setAttribute(
+      "style",
+      `position: relative;
+            width: ${divsSive}px;
+            height: ${divsSive}px;`
+    );
+    num.setAttribute("style", "position: absolute; top: 0; left: 0");
+
+    let opacity = 0;
+    let red = Math.floor(Math.random() * 255);
+    let blue = Math.floor(Math.random() * 255);
+    let green = Math.floor(Math.random() * 255);
+    div.addEventListener("mouseenter", (e) => {
+      opacity += 0.1;
+      e.target.style.backgroundColor = `rgba(${red}, ${blue}, ${green},${opacity})`;
+    });
+  }
 }
 
+button.addEventListener("click", () => {
+  let num = prompt("number of squares per side (maximim 100): ", 0);
 
-button.addEventListener('click', () => {
+  if (isNaN(num) || num <= 0 || num > 100) {
+    alert("Please enter a valid number between 1 and 100.");
+    return; // Don't create the grid if input is invalid
+  } else {
+    creatDivs(num);
+  }
+});
 
-    let num = prompt('number of squares per side (maximim 100): ', 0)
-
-    if (isNaN(num) || num <= 0 || num > 100) {
-        alert('Please enter a valid number between 1 and 100.');
-        return; // Don't create the grid if input is invalid
-    } else {
-        creatDivs(num)
-    }
-    
-})
-
-creatDivs(16)
-
-
-
+creatDivs(16);
